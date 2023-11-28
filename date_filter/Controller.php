eb083bb120eb9439f62a6d77bb18568029260571
andrejmakel <makel.andrej@gmail.com>
2023-11-28 13:10:40 +0100

    correction

--- date_filter/Controller.php
+++ date_filter/Controller.php
@@ -5,5 +5,5 @@ $query = Doklady::with(['team', 'sablona', 'dodavatel', 'typ', 'doc_source', 'my
 
 //select records when the filter is enabled
 if ($request->filled('from_date') && $request->filled('to_date') && $request->input('checkboxValue') == 1) {
-    $query = Doklady::whereBetween('date', [$request->from_date, $request->to_date]);
+    $query = Doklady::with(['team', 'sablona', 'dodavatel', 'typ', 'doc_source', 'my_email', 'period', 'predkontacia', 'clenenie_dph', 'clnenie_kv', 'currency', 'paid_from'])->select(sprintf('%s.*', (new Doklady)->table))->whereBetween('date', [$request->from_date, $request->to_date]);
 }
